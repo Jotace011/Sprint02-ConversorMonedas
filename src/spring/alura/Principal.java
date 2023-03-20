135b53d263e24a48f070d45a3fd81cac0f955a08
Jotace011 <jotace.026.3@gmail.com>
2023-03-20 12:32:41 -0500

    primer dia

--- src/spring/alura/Principal.java
+++ src/spring/alura/Principal.java
@@ -1,9 +1,17 @@
 package spring.alura;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
 
 	public static void main(String[] args) {
+
+		Object resp = JOptionPane.showInputDialog(null, "Seleccione la opcion", "Menu",1,null, new Object[] {
+				"Conversor de Monedas","Conversor de Temperatura"
+		}, "Seleccione");
+	
 		
 	}
+	
 	
 }
